0c0b3010ef2a2b8a20ad9efe2c185207e188e105
maxlinde <maximilian.linde.92@gmail.com>
2021-11-08 20:55:15 +0100

    Add multiple documentation files.

---
+++ R/infer_bf_precalculation_results.R
@@ -0,0 +1,24 @@
+#' Non-inferiority Bayes Factor Precalculation Results
+#' 
+#' Dataset that contains precalculated sample sizes with the
+#' non-inferiority Bayes factor method. The
+#' dataset contains the input and output values of these calculations.
+#' 
+#' @section Remark:
+#'   If none of the precalculated values suit your sample size
+#'   determination plan, than feel free the calculations with the R package
+#'   by using the \code{\link{ssp_infer_bf}} function.
+#' 
+#' @format A dataframe with 30690 rows and 9 variables:
+#' \describe{
+#'   \item{iterate}{numeric, unique id of iteration}
+#'   \item{tpr}{numeric, The long-run probability of obtaining a Bayes factor at least as high as the critical threshold favoring superiority, given Delta.}
+#'   \item{delta}{numeric, The expected population effect size.}
+#'   \item{thresh}{numeric, The Bayes factor threshold for inference. Either 3, 6, or 10.}
+#'   \item{prior_scale}{numeric, Scale of the Cauchy prior distribution. Either 1/sqrt(2), 1, or sqrt(2).}
+#'   \item{ni_margin}{non-negative numeric, The chosen non-inferiority margin.}
+#'   \item{n1}{numeric, The determined sample size per group.}
+#'   \item{tpr_out}{numeric, The TPR associated with the resulting sample sizes.}
+#'   \item{error_message}{character, The error message in case of an error.}
+#'}
+"infer_bf_precalculation_results"
