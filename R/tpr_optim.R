c7275d9a080a4daa7b3e9241e42b4a3333df34f6
sebastian-lortz <s.a.j.lortz@gmail.com>
2025-07-04 21:20:53 +0200

    tpr optim

--- R/tpr_optim.R
+++ R/tpr_optim.R
@@ -15,32 +15,41 @@
 #' @keywords internal
 tpr_optim <- function(fun, range, tpr, ...) {
   Ns = range
-  Res = c(fun(Ns[1], ...), 
+  Res = c(fun(Ns[1], ...),
           fun(Ns[2], ...))
-
+  
   if (tpr < min(Res)) {
     stop(paste0("Your chosen true positive rate (", tpr, ") level is already achieved for n = ", Ns[1], " with ", round(min(Res), 2), " true positive rate!"))
-    } else if (tpr > max(Res)) {
-      stop(paste0("Your chosen true positive rate (", tpr, ") level cannot be achieved for n < ", Ns[2], "!"))
-      }
+  } else if (tpr > max(Res)) {
+    stop(paste0("Your chosen true positive rate (", tpr, ") level cannot be achieved for n < ", Ns[2], "!"))
+  }
   
-  while ((min(Ns[Res>tpr]) - max(Ns[Res < tpr]))>1) {
+  # safeguard: check if endpoints exactly match and return result
+  hits <- which(abs(Res - tpr) < (.Machine$double.eps * 10))
+  if (length(hits)>0) {
+    idx <- hits[1]
+    return(list(
+      n1      = Ns[idx],
+      tpr_out = Res[idx]
+    ))
+  }
+  
+  while ((min(Ns[Res>=tpr]) - max(Ns[Res < tpr]))>1) {
     ResL = Res[which(Ns == max(Ns[Res < tpr]))]
-    ResH = Res[which(Ns == min(Ns[Res > tpr]))]
+    ResH = Res[which(Ns == min(Ns[Res >= tpr]))]
     NL = max(Ns[Res < tpr])
-    NH = min(Ns[Res > tpr])
+    NH = min(Ns[Res >= tpr])
     NewN = round((NH - NL) * ((tpr - ResL) / (ResH - ResL)) + NL)
     while (NewN %in% Ns) {
       NewN = ifelse(Res[length(Res)] > tpr, NewN - 1, NewN + 1)
-      }
+    }
     Ns = c(Ns, NewN)
     Res = c(Res, fun(Ns[length(Ns)], ...))
-    }
-  
+  }
   return(
     list(
-      n1 = min(Ns[Res > tpr]),
-      tpr_out = Res[which(Ns == min(Ns[Res > tpr]))]
-      )
+      n1 = min(Ns[Res >= tpr]),
+      tpr_out = Res[which(Ns == min(Ns[Res >= tpr]))]
     )
+  )
 }
